0381848624d9f93c32512777b10359ba9149e34d
andrea savignano <112469730+andreasavignano02@users.noreply.github.com>
2022-12-28 09:08:58 +0100

    start-model

---
+++ Polizia/Polizia/Models/Anagrafe.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Polizia.Models
+{
+    public class Anagrafe
+    {
+        public int IdAnagrafica { get; set; }
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+
+        public string Indirizzo { get; set; }
+
+        public string City { get; set; }
+
+        public string CodiceFiscale { get; set;}
+
+        public int IdVerbale { get; set; }
+        
+    }
+}
